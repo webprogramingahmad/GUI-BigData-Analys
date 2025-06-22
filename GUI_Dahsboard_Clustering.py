60a8851d09ec360fd60f696053d18a0319482696
webprogramingahmad <ahmadnurazis96@gmail.com>
2025-06-23 00:23:44 +0700

    menambahkan seaborn

--- GUI_Dahsboard_Clustering.py
+++ GUI_Dahsboard_Clustering.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import seaborn as sns
 import re
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.cluster import KMeans
